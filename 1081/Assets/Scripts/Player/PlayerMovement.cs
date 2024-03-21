f538ae75c8bd42098e3c8283150e8161e5ad7131
Dave Noceda <dbnoceda@up.edu.ph>
2024-03-21 15:48:59 +0800

    Add Animation to Player Walk and Idle

--- 1081/Assets/Scripts/Player/PlayerMovement.cs
+++ 1081/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,19 +5,33 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private int speed = 5;
+    [SerializeField] private float speed = 3f;
 
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnMovement(InputValue value)
     {
         movement = value.Get<Vector2>();
+
+        if (movement.x != 0 || movement.y != 0)
+        {
+            animator.SetFloat("X", movement.x);
+            animator.SetFloat("Y", movement.y);
+
+            animator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+        }
     }
 
     private void FixedUpdate()
