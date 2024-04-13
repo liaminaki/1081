9e4b0b70648a863a8db03a83c33be6e3da629c09
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2024-04-13 19:59:44 +0800

    Add C1 Postgame with EndTrigger script

---
+++ 1081/Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class EndTrigger : MonoBehaviour
+{
+    // References to the timelines
+    public PlayableDirector MainTimeline;
+    
+    // The renamed reference to the post-game timeline is now called Timeline2
+    public PlayableDirector Timeline2;
+
+    // A flag to track whether the event has been handled
+    private bool eventHandled = false;
+
+    // This method is called at the start of the script's life
+    void Start()
+    {
+        // Play the MainTimeline at the beginning of the game
+        if (MainTimeline != null)
+        {
+            MainTimeline.Play();
+        }
+        
+        // Ensure Timeline2 is stopped at the start of the game
+        if (Timeline2 != null)
+        {
+            Timeline2.Stop();
+        }
+    }
+
+    // This method is called when another collider enters the collider attached to this game object
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // Check if the event has already been handled
+        if (eventHandled)
+        {
+            // Exit the method if the event has been handled to avoid duplicate execution
+            return;
+        }
+
+        // Check if the collided object is tagged as "Player"
+        if (collider.CompareTag("Player"))
+        {
+            // Log the collision for debugging purposes
+            Debug.Log("Player collided with the end trigger.");
+
+            // Stop the MainTimeline if it's currently playing
+            if (MainTimeline != null && MainTimeline.state == PlayState.Playing)
+            {
+                MainTimeline.Stop();
+                Debug.Log("MainTimeline stopped.");
+            }
+
+            // Play Timeline2
+            if (Timeline2 != null)
+            {
+                Timeline2.Play();
+                Debug.Log("Timeline2 started.");
+            }
+
+            // Set the eventHandled flag to true to prevent future execution
+            eventHandled = true;
+        }
+    }
+}
+
+
