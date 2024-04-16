6eb1b8938391a709d5993c786196a00a8ded5278
Dave Noceda <dbnoceda@up.edu.ph>
2024-04-16 11:57:47 +0800

    Initial Enemy Backend

--- 1081/Assets/Scripts/Enemy/EnemyAI.cs
+++ 1081/Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject PointA;
+    public GameObject PointB;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+    public float speed;
+
+    void Start (){
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = PointB.transform;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update (){
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == PointB.transform){
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else{
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        float xVelocity = rb.velocity.x;
+        float yVelocity = rb.velocity.y;
+        anim.SetFloat("X", xVelocity);
+        anim.SetFloat("Y", yVelocity);
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 1f && currentPoint == PointB.transform){
+            currentPoint = PointA.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 1f && currentPoint == PointA.transform){
+            currentPoint = PointB.transform;
+        }
+    }
+
+    private void OnDrawGizmos(){
+        Gizmos.DrawWireSphere(PointA.transform.position, 1f);
+        Gizmos.DrawWireSphere(PointB.transform.position, 1f);
+        Gizmos.DrawLine(PointA.transform.position, PointB.transform.position);
     }
 }
---
+++ 1081/Assets/Scripts/Enemy/FieldOfView.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float radius = 5f;
+    [Range (1, 360)] public float angle = 45f;
+    public LayerMask targetLayer;
+    public LayerMask obstructionLayer;
+
+    public GameObject playerRef;
+
+    public bool CanSeePlayer {get; private set;}
+
+    void Start(){
+        GameObject [] playerObjects = GameObject.FindGameObjectsWithTag("Player");
+        if(playerObjects.Length > 0){
+            playerRef = playerObjects[0];
+        }
+    }
+
+    private IEnumerator FOVCheck(){
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while (true){
+            yield return wait;
+            FOV();
+        }
+    }
+
+    private void FOV(){
+        Collider2D[] rangeCheck = Physics2D.OverlapCircleAll(transform.position, radius, targetLayer);
+
+        if(rangeCheck.Length > 0){
+            Transform target = rangeCheck[0].transform;
+            Vector2 directionToTarget = (target.position = transform.position).normalized;
+
+            if (Vector2.Angle(transform.up, directionToTarget) < angle/2){
+                float distanceToTarget = Vector2.Distance(transform.position, target.position);
+
+                if(!Physics2D.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionLayer))
+                    CanSeePlayer = true;
+                else
+                    CanSeePlayer = false;
+            }
+            else
+                CanSeePlayer = false;
+        }
+        else if (CanSeePlayer)
+            CanSeePlayer = false;
+    }
+}
